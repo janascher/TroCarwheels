7b8536b3a5218051235d4962c7fd42acb1490094
LucasCscBks <lucas_reisgod@yahoo.com.br>
2022-10-06 23:30:32 -0300

    update home.css , module home.js

--- views/modules/home.js
+++ views/modules/home.js
@@ -77,6 +77,7 @@ export default function home() {
             </div>
         </div>
     </section>
+    <div id="divide"></div>
     <section id="info">
         <div id="info_container">
             <div>
